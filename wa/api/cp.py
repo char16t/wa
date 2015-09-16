223396c931fbb8c04e9c3da55404d81fffdb6190
Valeriy Manenkov <v.manenkov@gmail.com>
2015-09-16 10:03:11 +0300

    Implementation cp api-method for single files

--- wa/api/cp.py
+++ wa/api/cp.py
@@ -1,7 +1,11 @@
 import sys
+import shutil
 
 def main(args):
-    pass
+    if len(args) != 2:
+        print("cp: Usage: cp <infile> <outfile>")
+        sys.exit(1)
+    shutil.copyfile(args[0], args[1])
 
 if __name__ == "__main__":
     main(sys.argv[1:])
