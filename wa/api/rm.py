9f50bd004e9e523e92f395e02e90647704bfce3a
Valeriy Manenkov <v.manenkov@gmail.com>
2015-09-16 10:19:00 +0300

    Implementation rm api-method

--- wa/api/rm.py
+++ wa/api/rm.py
@@ -1,7 +1,13 @@
 import sys
+import os
+import shutil
 
 def main(args):
-    pass
+    for arg in args:
+        if os.path.isfile(os.path.abspath(arg)):
+            os.remove(os.path.abspath(arg))
+        if os.path.isdir(os.path.abspath(arg)):
+            shutil.rmtree(os.path.abspath(arg))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
