b0c8e381b890a02d71cadfdb6c2d119ee0295dc5
Valeriy Manenkov <v.manenkov@gmail.com>
2015-09-19 17:39:24 +0300

    Implementation of input api-method

--- wa/api/input.py
+++ wa/api/input.py
@@ -3,10 +3,31 @@ import sys
 def main(vars):
     import os
     import pickle
+    import hashlib
+    
+    pickle_hash = hashlib.md5()
+    pickle_hash.update(os.getcwd().encode())
+    pickle_dump = os.path.join(os.path.expanduser("~"), ".wa", "_wa_temp", pickle_hash.hexdigest())
+
+    if not os.path.exists(os.path.join(os.path.expanduser("~"), ".wa", "_wa_temp")):
+        os.makedirs(os.path.join(os.path.expanduser("~"), ".wa", "_wa_temp"))
+    
+    if not os.path.exists(os.path.join(os.path.expanduser("~"), ".wa", "_wa_temp", pickle_hash.hexdigest())):
+        with open(pickle_dump, "w") as f:
+            f.close()
+
     for var in vars:
-        variables = {}
-        var_value = input(var + ": ")
-        variables[var] = var_value
+        try:
+            variables = pickle.load(open(pickle_dump, "rb"))
+            print(variables)
+            var_value = input(var + ": ")
+            variables[var] = var_value
+            pickle.dump(variables, open(pickle_dump, "wb"))
+        except EOFError:
+            variables = {} 
+            var_value = input(var + ": ")
+            variables[var] = var_value
+            pickle.dump(variables, open(pickle_dump, "wb"))
 
 if __name__ == "builtins":
     main(sys.argv[1:])
