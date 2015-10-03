0c686fea9ba376f3ef868f07ab2c5f92d4ad4aeb
Valeriy Manenkov <v.manenkov@gmail.com>
2015-10-03 17:47:50 +0300

    Setup script: Version now in version.py file, long_description now in README.rst

--- setup.py
+++ setup.py
@@ -3,13 +3,24 @@ try:
 except ImportError:
     from distutils.core import setup
 
+__version__ = None
+
+with open(local_file("wa/version.py")) as o:
+    exec(o.read())
+
+assert __version__ is not None
+
+with open('README.rst') as f:
+    long_description = f.read()
+
 config = {
     'name': 'wa',
     'author': 'Valeriy Manenkov',
     'author_email': 'v.manenkov@gmail.com',
     'description': 'Workflow automation tool',
+    'long_description': long_description,
     'license': 'MIT',
-    'version': '0.1.0',
+    'version': __version__,
     'url': 'https://github.com/char16t/wa',
     'download_url': 'https://github.com/char16t/wa/archive/master.tar.gz',
     'install_requires': ['pyyaml', 'Jinja2'],
---
+++ wa/version.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+
+from __future__ import division, print_function, absolute_import
+
+__version_info__ = (0, 1, 0)
+__version__ = u'.'.join(map(str, __version_info__))
